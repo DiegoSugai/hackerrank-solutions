fbe7962c5b21eca50e2707f228d7a0649cc77fb2
Diego Spagnuolo Sugai <140738128+DiegoSugai@users.noreply.github.com>
2026-09-05 23:23:18 -0300

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 22727-write-a-function-Hackerrank/22727-write-a-function.py
@@ -0,0 +1,10 @@
+def is_leap(year):
+    leap = False
+    
+    return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
+
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
